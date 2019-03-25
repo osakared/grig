0d0217a5ba1059e597a20f2b0bfa0dffd65bfe34
Thomas J. Webb <thomas@osakared.io>
2019-03-24 19:46:49 -0700

    Work on MonoSynth example

--- examples/MonoSynth/src/MonoSynth.hx
+++ examples/MonoSynth/src/MonoSynth.hx
@@ -1,5 +1,6 @@
 package;
 
+import grig.audio.AudioInterface;
 import grig.midi.MidiIn;
 import grig.midi.MidiMessage;
 import grig.synth.fmsynth.FMSynth;
@@ -9,17 +10,24 @@ import haxe.Resource;
 class MonoSynth
 {
     var midiIn:MidiIn;
+    var audioInterface:AudioInterface;
 	var synth:FMSynth;
-
-	public function new()
+    
+    private function audioCallback(input:grig.audio.AudioBuffer, output:grig.audio.AudioBuffer, sampleRate:Float, audioStreamInfo:grig.audio.AudioStreamInfo)
     {
-		// synth = new FMSynth(44100.0, 8, 8);
+        synth.render(output.channels[0], output.channels[1]);
+    }
+
+    private function midiCallback(midiMessage:MidiMessage, delta:Float)
+    {
+        synth.parseMidi(midiMessage.toBytes());
+    }
+
+    private function initMidi()
+    {
         trace(MidiIn.getApis());
         var midiIn_ = new MidiIn(grig.midi.Api.Unspecified);
-        midiIn_.setCallback(function (midiMessage:MidiMessage, delta:Float) {
-            trace(midiMessage.messageType);
-            trace(delta);
-        });
+        midiIn_.setCallback(midiCallback);
         midiIn_.getPorts().handle(function(outcome) {
             switch outcome {
                 case Success(ports):
@@ -37,6 +45,44 @@ class MonoSynth
                     trace(error);
             }
         });
+    }
+
+    private function initAudio()
+    {
+        trace(AudioInterface.getApis());
+        var audioInterface_ = new AudioInterface();
+        var ports = audioInterface_.getPorts();
+        var options:grig.audio.AudioInterfaceOptions = {};
+        trace(ports.length);
+        for (port in ports) {
+            if (port.isDefaultOutput) {
+                options.outputNumChannels = port.maxOutputChannels;
+                options.outputPort = port.portID;
+                options.sampleRate = port.defaultSampleRate;
+                options.inputNumChannels = 0;
+                options.outputLatency = port.defaultLowOutputLatency;
+            }
+            break;
+        }
+        audioInterface_.setCallback(audioCallback);
+        audioInterface_.openPort(options).handle(function(audioOutcome) {
+            switch audioOutcome {
+                case Success(_):
+                    trace(audioInterface_.isOpen);
+                    audioInterface = audioInterface_;
+                case Failure(error):
+                    trace(error);
+            }
+        });
+    }
+
+	public function new()
+    {
+        initMidi();
+        initAudio();
+
+        synth = new FMSynth(44100.0, 8, 8);
+        loadPatch('lead_pad.fmp');
         mainLoop();
 	}
 
@@ -57,6 +103,7 @@ class MonoSynth
             var command = stdin.readLine();
             if (command.toLowerCase() == 'quit') {
                 if (midiIn != null) midiIn.closePort();
+                if (audioInterface != null) audioInterface.closePort(); 
                 return;
             }
         }
@@ -67,30 +114,5 @@ class MonoSynth
     {
         var monoSynth = new MonoSynth();
     }
-
-	// override function onSample( buf : haxe.io.Float32Array ) {
-	// 	if (left == null || left.length != buf.length) {
-	// 		left = new AudioChannel(buf.length, 44100);
-	// 		right = new AudioChannel(buf.length, 44100);
-	// 	}
-	// 	else {
-	// 		left.clear();
-	// 		right.clear();
-	// 	}
-	// 	synth.render(left, right);
-	// 	for( i in 0...buf.length ) {
-	// 		buf[i] = left.get(i);
-	// 	}
-	// }
-
-	// public function noteOn(note:Int, velocity:Int = 64)
-	// {
-	// 	synth.noteOn(note, velocity);
-	// }
-
-	// public function noteOff(note:Int)
-	// {
-	// 	synth.noteOff(note);
-	// }
 
 }

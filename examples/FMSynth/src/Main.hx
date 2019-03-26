1c607b728e266dbd55b5357b258989afffc030ce
Thomas J. Webb <thomas@osakared.com>
2019-03-25 20:07:02 -0700

    Experimenting with simple single-oscillator synth and using fmsynth

---
+++ examples/FMSynth/src/Main.hx
@@ -0,0 +1,144 @@
+package;
+
+import grig.audio.AudioInterface;
+import grig.midi.MidiIn;
+import grig.midi.MidiMessage;
+import grig.synth.fmsynth.FMSynth;
+import haxe.io.BytesInput;
+import haxe.Resource;
+
+#if (target.threaded)
+import sys.thread.Mutex;
+#end
+
+class Main
+{
+    var midiIn:MidiIn;
+    var audioInterface:AudioInterface;
+	var synth:FMSynth;
+
+    #if (target.threaded)
+    var m:Mutex;
+    #end
+
+    private inline function guardSynth(f:()->Void):Void
+    {
+        #if (target.threaded)
+        m.acquire();
+        #end
+        f();
+        #if (target.threaded)
+        m.release();
+        #end
+    }
+    
+    private function audioCallback(input:grig.audio.AudioBuffer, output:grig.audio.AudioBuffer, sampleRate:Float, audioStreamInfo:grig.audio.AudioStreamInfo)
+    {
+        guardSynth(function() {
+            synth.render(output.channels[0], output.channels[1]);
+        });
+    }
+
+    private function midiCallback(midiMessage:MidiMessage, delta:Float)
+    {
+        guardSynth(function() {
+            synth.parseMidi(midiMessage.toBytes());
+        });
+    }
+
+    private function initMidi()
+    {
+        trace(MidiIn.getApis());
+        var midiIn_ = new MidiIn(grig.midi.Api.Unspecified);
+        midiIn_.setCallback(midiCallback);
+        midiIn_.getPorts().handle(function(outcome) {
+            switch outcome {
+                case Success(ports):
+                    trace(ports);
+                    midiIn_.openPort(0, 'grig.midi').handle(function(midiOutcome) {
+                        switch midiOutcome {
+                            case Success(_):
+                                trace('Started midi input');
+                                midiIn = midiIn_;
+                            case Failure(error):
+                                trace(error);
+                        }
+                    });
+                case Failure(error):
+                    trace(error);
+            }
+        });
+    }
+
+    private function initAudio()
+    {
+        trace(AudioInterface.getApis());
+        var audioInterface_ = new AudioInterface();
+        var ports = audioInterface_.getPorts();
+        var options:grig.audio.AudioInterfaceOptions = {};
+        trace(ports.length);
+        for (port in ports) {
+            if (port.isDefaultOutput) {
+                options.outputNumChannels = port.maxOutputChannels;
+                options.outputPort = port.portID;
+                options.sampleRate = port.defaultSampleRate;
+                options.inputNumChannels = 0;
+                options.outputLatency = port.defaultLowOutputLatency;
+                break;
+            }
+        }
+        audioInterface_.setCallback(audioCallback);
+        audioInterface_.openPort(options).handle(function(audioOutcome) {
+            switch audioOutcome {
+                case Success(_):
+                    trace(audioInterface_.isOpen);
+                    audioInterface = audioInterface_;
+                case Failure(error):
+                    trace(error);
+            }
+        });
+    }
+
+	public function new()
+    {
+        #if (target.threaded)
+        m = new Mutex();
+        #end
+        initMidi();
+        initAudio();
+
+        synth = new FMSynth(44100.0, 8, 8);
+        loadPatch('lead_pad.fmp');
+        mainLoop();
+	}
+
+	public function loadPatch(name:String)
+	{
+		var patchBytes = Resource.getBytes(name);
+        synth.loadLibFMSynthPreset(new BytesInput(patchBytes));
+	}
+
+    private function mainLoop()
+    {
+        #if (sys && !nodejs)
+        var stdout = Sys.stdout();
+        var stdin = Sys.stdin();
+        // Using Sys.getChar() unfortunately fucks up the output
+        stdout.writeString('quit[enter] to quit\n');
+        while (true) {
+            var command = stdin.readLine();
+            if (command.toLowerCase() == 'quit') {
+                if (midiIn != null) midiIn.closePort();
+                if (audioInterface != null) audioInterface.closePort(); 
+                return;
+            }
+        }
+        #end
+    }
+
+    static public function main()
+    {
+        var fmSynth = new Main();
+    }
+
+}
--- examples/MonoSynth/src/MonoSynth.hx
+++ examples/MonoSynth/src/MonoSynth.hx
@@ -1,118 +1,45 @@
 package;
 
-import grig.audio.AudioInterface;
-import grig.midi.MidiIn;
-import grig.midi.MidiMessage;
-import grig.synth.fmsynth.FMSynth;
-import haxe.io.BytesInput;
-import haxe.Resource;
-
 class MonoSynth
 {
-    var midiIn:MidiIn;
-    var audioInterface:AudioInterface;
-	var synth:FMSynth;
-    
-    private function audioCallback(input:grig.audio.AudioBuffer, output:grig.audio.AudioBuffer, sampleRate:Float, audioStreamInfo:grig.audio.AudioStreamInfo)
+    private var heldNotes:Array<Int> = new Array<Int>();
+    private var sampleRate:Float;
+    private var phase:Float = 0.0;
+
+    public function new(_sampleRate:Float)
     {
-        synth.render(output.channels[0], output.channels[1]);
+        sampleRate = _sampleRate;
     }
 
-    private function midiCallback(midiMessage:MidiMessage, delta:Float)
+    public function parseMidi(midiMessage:grig.midi.MidiMessage):Void
     {
-        synth.parseMidi(midiMessage.toBytes());
-    }
-
-    private function initMidi()
-    {
-        trace(MidiIn.getApis());
-        var midiIn_ = new MidiIn(grig.midi.Api.Unspecified);
-        midiIn_.setCallback(midiCallback);
-        midiIn_.getPorts().handle(function(outcome) {
-            switch outcome {
-                case Success(ports):
-                    trace(ports);
-                    midiIn_.openPort(1, 'grig.midi').handle(function(midiOutcome) {
-                        switch midiOutcome {
-                            case Success(_):
-                                trace('Started midi input');
-                                midiIn = midiIn_;
-                            case Failure(error):
-                                trace(error);
-                        }
-                    });
-                case Failure(error):
-                    trace(error);
-            }
-        });
-    }
-
-    private function initAudio()
-    {
-        trace(AudioInterface.getApis());
-        var audioInterface_ = new AudioInterface();
-        var ports = audioInterface_.getPorts();
-        var options:grig.audio.AudioInterfaceOptions = {};
-        trace(ports.length);
-        for (port in ports) {
-            if (port.isDefaultOutput) {
-                options.outputNumChannels = port.maxOutputChannels;
-                options.outputPort = port.portID;
-                options.sampleRate = port.defaultSampleRate;
-                options.inputNumChannels = 0;
-                options.outputLatency = port.defaultLowOutputLatency;
-            }
-            break;
+        if (midiMessage.messageType == NoteOn) {
+            heldNotes.push(midiMessage.byte2);
         }
-        audioInterface_.setCallback(audioCallback);
-        audioInterface_.openPort(options).handle(function(audioOutcome) {
-            switch audioOutcome {
-                case Success(_):
-                    trace(audioInterface_.isOpen);
-                    audioInterface = audioInterface_;
-                case Failure(error):
-                    trace(error);
-            }
-        });
-    }
-
-	public function new()
-    {
-        initMidi();
-        initAudio();
-
-        synth = new FMSynth(44100.0, 8, 8);
-        loadPatch('lead_pad.fmp');
-        mainLoop();
-	}
-
-	public function loadPatch(name:String)
-	{
-		var patchBytes = Resource.getBytes(name);
-        synth.loadLibFMSynthPreset(new BytesInput(patchBytes));
-	}
-
-    private function mainLoop()
-    {
-        #if (sys && !nodejs)
-        var stdout = Sys.stdout();
-        var stdin = Sys.stdin();
-        // Using Sys.getChar() unfortunately fucks up the output
-        stdout.writeString('quit[enter] to quit\n');
-        while (true) {
-            var command = stdin.readLine();
-            if (command.toLowerCase() == 'quit') {
-                if (midiIn != null) midiIn.closePort();
-                if (audioInterface != null) audioInterface.closePort(); 
-                return;
-            }
+        else if (midiMessage.messageType == NoteOff) {
+            heldNotes.remove(midiMessage.byte2);
         }
-        #end
     }
 
-    static public function main()
+    public function render(output:grig.audio.AudioBuffer):Void
     {
-        var monoSynth = new MonoSynth();
+        if (heldNotes.length == 0) {
+            output.clear();
+            return;
+        }
+        if (output.channels.length == 0) {
+            return;
+        }
+        // High note priority
+        heldNotes.sort(function(x, y):Int {
+            if (x < y) return 1;
+            else if (x > y) return -1;
+            else return 0;
+        });
+        var frequency:Float = (440.0 / 32.0) * Math.pow(2.0, ((heldNotes[0] - 9.0) / 12.0));
+        for (i in 0...output.channels[0].length) {
+            output.channels[0][i] = Math.sin(phase * Math.PI);
+            phase += frequency / sampleRate;
+        }
     }
-
 }

c5b23d2d214ed80f307d5e7366ad01a2c959f49b
Thomas J. Webb <thomas@osakared.com>
2019-04-30 08:15:05 -0700

    Switched to using entire block to fade in/out

--- examples/MonoSynth/src/MonoSynth.hx
+++ examples/MonoSynth/src/MonoSynth.hx
@@ -1,15 +1,22 @@
 package;
 
+enum Stage
+{
+    Attack;
+    Sustain;
+    Release;
+}
+
 class MonoSynth
 {
     private var heldNotes:Array<Int> = new Array<Int>();
     private var sampleRate:Float;
 
     private var phase:Float = 0.0;
-    private var fadeSamples:Int = 4000; // Used for attack and release, just to prevent clicks
-    private var positionInFade:Int = 0; // Sample position in the fade
+    private var phase2:Float = 0.0;
     private var currentNote:Null<Int>;
     private var playing:Bool = false;
+    private var stage:Stage = Attack;
 
     public function new(_sampleRate:Float)
     {
@@ -36,33 +43,32 @@ class MonoSynth
             return;
         }
         if (heldNotes.length > 0) {
-            if (!playing) positionInFade = 0;
+            if (!playing) stage = Attack;
+            else stage = Sustain;
             playing = true;
             // Last note priority
             currentNote = heldNotes[heldNotes.length - 1];
         }
         else {
+            stage = Release;
             playing = false;
         }
         var frequency:Float = (440.0 / 32.0) * Math.pow(2.0, ((currentNote - 9.0) / 12.0));
+        var frequency2:Float = frequency * 1.01;
         for (i in 0...output.channels[0].length) {
             // output.channels[0][i] = Math.sin(phase * Math.PI);
-            output.channels[0][i] = phase > 0.1 ? 1.0 : 0.0;
-            // Fading in
-            if (playing && positionInFade < fadeSamples) {
-                output.channels[0][i] *= positionInFade / fadeSamples;
-                positionInFade++;
+            output.channels[0][i] = phase * 0.7 + phase2 * 0.7;//phase > 0.5 ? 1.0 : 0.0;
+            if (stage == Attack) {
+                output.channels[0][i] *= i / output.channels[0].length;
             }
-            else if (!playing && positionInFade > 0) {
-                output.channels[0][i] *= positionInFade / fadeSamples;
-                positionInFade--;
-            }
-            else if (!playing && positionInFade == 0) {
-                currentNote = null;
-                break;
+            else if (stage == Release) {
+                output.channels[0][i] *= (output.channels[0].length - i) / output.channels[0].length;
             }
             phase += frequency / sampleRate;
+            phase2 += frequency2 / sampleRate;
             while (phase > 1.0) phase -= 1.0;
+            while (phase2 > 1.0) phase2 -= 1.0;
         }
+        if (!playing) currentNote = null;
     }
 }
